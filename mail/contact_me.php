c59e20f5806013d34f8b6a2b38d233cd00870d7a
nforleo <nickm140mc@gmail.com>
2016-06-28 15:05:33 -0400

    updated code
    attempt 1 at trying to make phone number optional.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,13 +2,17 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
+//   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
+   
+ if(empty($_POST['phone'])) {
+	$phone = "NULL";
+ }
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
