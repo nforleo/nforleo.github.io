39e778a7646347c56747e14ee0d995ec696cbc8e
nforleo <nickm140mc@gmail.com>
2016-06-29 08:49:54 -0400

    contact form
    commented out return true on last line

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -23,5 +23,5 @@ $email_body = "You have received a new message from your website contact form.\n
 $headers = "From: noreply@nforleo.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+//return true;			
 ?>
